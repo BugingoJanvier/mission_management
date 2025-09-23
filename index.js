4d76f99115378ee7b6cfe1d971e200e954964f68
BUGINGO Janvier <66186428+BugingoJanvier@users.noreply.github.com>
2025-09-23 12:18:11 +0200

    remove linting conflict

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 import express from 'express';
+import dotenv from 'dotenv';
 
 const app = express();
-const PORT = 3000;
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
 
 // Middleware to parse JSON
 app.use(express.json());
 
 // Basic route
-app.get('/', (req, res) => {
+app.get('/', (_, res) => {
   res.send('Mission Management API is running');
 });
 
-// Start server
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
